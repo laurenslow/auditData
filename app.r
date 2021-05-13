8c1bad4c2fedc644bd4388500e663b8b1b1556b4
laurenslow <lauren.s.low@gmail.com>
2021-05-13 13:12:11 -0400

    changed theme and added to plot

--- app.r
+++ app.r
@@ -7,15 +7,16 @@ library(ISLR)
 library(tigris)
 library(leaflet)
 library(stringr)
+library(shinythemes)
 
-
-# Define UI for application that draws a histogram
 ui <- fluidPage(
+  
+  # shiny app theme
+  theme = shinytheme("flatly"),
   
   # Application title
   titlePanel("Association Between Income, Race and Tax Audit Rates"),
   
-  # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
       checkboxGroupInput(inputId = "scatter_plot",
@@ -25,33 +26,27 @@ ui <- fluidPage(
                                      "Higher level polynomial" = "3"),
                          selected = NULL),
       
-    #   selectInput("select",
-    #               "State",
-    #               choices = sort(unique(joined_cnty$state.x)),
-    #               selected = "All"),
-    # 
-    # ),
-    
-    selectInput("select",
-                "State",
-                choices = append(c("All"), sort(unique(joined_cnty$state.x))),
-                selected = "All"),
+      selectInput("select",
+                  "State",
+                  choices = append(c("All"), sort(unique(joined_cnty$state.x))),
+                  selected = "All"),
     ),
     
-    # Show a plot of the generated distribution
     mainPanel(tabsetPanel(type = "tabs", 
-                  tabPanel("Graph" , plotOutput("scatter_plot")),
-                  tabPanel("Map" , leafletOutput("map_plot"))
-                  ))
-    )
+                          tabPanel("Graph", plotOutput("scatter_plot")),
+                          tabPanel("Map", leafletOutput("map_plot"))
+    )))
+  
 )
 
 
 
 
-# Define server logic required to draw a histogram
+
+# define server logic to draw plot and map
 server <- function(input, output) {
-  
+ 
+  # load data 
   audits <- here::here("data/cleaned/auditsData_2019.04.03.csv") %>%
     read_csv()
   race_county <- here::here("data/cleaned/race_eth_county.csv") %>%
@@ -61,7 +56,8 @@ server <- function(input, output) {
   joined <- full_join(audits, race_county, by = "fips") %>%
     full_join(., incomes, by = "fips") %>% 
     select(fips, state.x, county, n_returns, estimated_exams, audit_rate, white, black, indian, asian, hawaiian, other, two, non_his, hispanic, median_income)
-  
+ 
+  # wrangle data 
   joined <- joined %>%
     mutate(non_white = black + indian + asian + hawaiian + other + two, 
            pred_white = as.logical(ifelse(white >= non_white, 1, 0)))
@@ -77,27 +73,38 @@ server <- function(input, output) {
   pal <- colorNumeric(palette = "YlGnBu", domain = joined_cnty$audit_rate)
   
   
+  # create output plots
   output$scatter_plot <- renderPlot({
     
-    data <- if ("All" == input$select) {
+    #if all of the states are selected show joined_cnty data set else filter by the selected state
+    sort_state <- if ("All" == input$select) {
       joined_cnty
     } else {
       joined_cnty %>% filter(state.x == input$select)
     }
     
+    # make sort_state something the user can iotneract with
     react <- reactive({
-      data
+      sort_state
     })
   
-    
+    # draw baseline scatter plot 
     base_plot <- ggplot(data = react(), aes(x = median_income, y = audit_rate, color = pred_white)) +
-      geom_point() 
+      geom_point() +
+      scale_x_continuous(labels = scales::dollar) +
+      scale_y_continuous(labels = function(x) paste0(x, '%')) +
+      scale_color_discrete(labels = c("People of Color", "White", "Racial Data not Available")) +
+      labs(x = "Median Income",
+           y = "Percent of Tax Returns Audited",
+           title = "",
+           subtitle = "Each point represents one US county", 
+           color = "Predominant Racial Identity")
 
-    
+    # conditionals to add regression lines
     if(is.null(input$scatter_plot)) {out <- base_plot;
     }
     else if(identical(c("1"), input$scatter_plot)) {out <- base_plot + 
-      geom_smooth(method = "lm", color = "#f9766e");
+      geom_smooth(method = lm, color = "#f9766e");
     }
     else if(identical(c("2"), input$scatter_plot)) {out <- base_plot + 
       stat_smooth(method = lm, formula = y ~ poly(x, 2, raw = TRUE), color = "#7caf00");
@@ -106,11 +113,11 @@ server <- function(input, output) {
       stat_smooth(method = lm, formula = y ~ poly(x, 3, raw = TRUE), color = "#c77cff");
     }
     else if(identical(c("1", "2"), input$scatter_plot)) {out <- base_plot +
-            geom_smooth(method = "lm", color = "#f9766e") +
+            geom_smooth(method = lm, color = "#f9766e") +
             stat_smooth(method = lm, formula = y ~ poly(x, 2, raw = TRUE), color = "#7caf00");
     }
     else if(identical(c("1", "3"), input$scatter_plot)) {out <- base_plot +
-      geom_smooth(method = "lm", color = "#f9766e") +
+      geom_smooth(method = lm, color = "#f9766e") +
       stat_smooth(method = lm, formula = y ~ poly(x, 3, raw = TRUE), color = "#c77cff");
     }
     else if(identical(c("2", "3"), input$scatter_plot)) {out <- base_plot +
@@ -118,12 +125,13 @@ server <- function(input, output) {
       stat_smooth(method = lm, formula = y ~ poly(x, 3, raw = TRUE), color = "#c77cff");
     }
     else if(identical(c("1", "2", "3"), input$scatter_plot)) {out <- base_plot +
-      geom_smooth(method = "lm", color = "#f9766e") +
+      geom_smooth(method = lm, color = "#f9766e") +
       stat_smooth(method = lm, formula = y ~ poly(x, 2, raw = TRUE), color = "#7caf00") +
       stat_smooth(method = lm, formula = y ~ poly(x, 3, raw = TRUE), color = "#c77cff");
     }
     
- out
+    # display out (baseline scatter) plot
+    out
     
     
     
@@ -132,6 +140,7 @@ server <- function(input, output) {
   
   output$map_plot <- renderLeaflet({
     
+    # draw map in leaflet
     leaflet(joined_cnty) %>%
       addTiles() %>%
       addPolygons(fillColor = ~pal(audit_rate), 
